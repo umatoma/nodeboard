043a629395c512f410cc5b6e515d0d4b1b7a18d3
umatoma <umatomakun@gmail.com>
2015-11-24 23:10:16 +0900

    create stop mongodb task

--- gulpfile.js
+++ gulpfile.js
@@ -6,16 +6,35 @@ gulp.task('default', function() {
   // place code for your default task here
 });
 
+/**
+* Compile sass files
+*/
 gulp.task('sass', function() {
   gulp.src('public/stylesheets/*.sass')
     .pipe(sass())
     .pipe(gulp.dest('public/stylesheets'));
 });
 
+/**
+* Start MongoDB
+*/
 gulp.task('mongo.start', function(cb) {
   exec('mongod --config conf/mongod.conf --fork', function(err, stdout, stderr) {
-    console.log('stdout: ' + stdout);
-    console.log('stderr: ' + stderr);
+    console.log('[stdout] ' + stdout);
+    console.log('[stderr] ' + stderr);
+
+    if (err) return cb(err);
+    cb();
+  });
+});
+
+/**
+* Stop MongoDB
+*/
+gulp.task('mongo.stop', function(cb) {
+  exec('kill `pgrep mongod`', function(err, stdout, stderr) {
+    console.log('[stdout] ' + stdout);
+    console.log('[stderr] ' + stderr);
 
     if (err) return cb(err);
     cb();
