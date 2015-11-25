7cb2b931e69feb66fc6b8d91a544e42190c61e56
umatoma <umatomakun@gmail.com>
2015-11-25 23:03:22 +0900

    refactor gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var exec = require('child_process').exec;
+var eslint = require('gulp-eslint');
+var gutil = require('gulp-util');
 
 gulp.task('default', function() {
   // place code for your default task here
@@ -10,7 +12,7 @@ gulp.task('default', function() {
 * Compile sass files
 */
 gulp.task('sass', function() {
-  gulp.src('public/stylesheets/*.sass')
+  return gulp.src('public/stylesheets/*.sass')
     .pipe(sass())
     .pipe(gulp.dest('public/stylesheets'));
 });
@@ -19,11 +21,15 @@ gulp.task('sass', function() {
 * Start MongoDB
 */
 gulp.task('mongo.start', function(cb) {
-  exec('mongod --config conf/mongod.conf --fork', function(err, stdout, stderr) {
-    console.log('[stdout] ' + stdout);
-    console.log('[stderr] ' + stderr);
-
-    if (err) return cb(err);
+  var child = exec('mongod --config conf/mongod.conf --fork');
+  child.stdout.on('data', function(data) {
+    gutil.log(data);
+  });
+  child.stderr.on('data', function(data) {
+    gutil.log(gutil.colors.red(data));
+  });
+  child.on('close', function(code) {
+    gutil.log('closing code:', gutil.colors.cyan(code));
     cb();
   });
 });
@@ -32,11 +38,29 @@ gulp.task('mongo.start', function(cb) {
 * Stop MongoDB
 */
 gulp.task('mongo.stop', function(cb) {
-  exec('kill `pgrep mongod`', function(err, stdout, stderr) {
-    console.log('[stdout] ' + stdout);
-    console.log('[stderr] ' + stderr);
-
-    if (err) return cb(err);
+  var child = exec('kill `pgrep mongod`');
+  child.stdout.on('data', function(data) {
+    gutil.log(data);
+  });
+  child.stderr.on('data', function(data) {
+    gutil.log(gutil.colors.red(data));
+  });
+  child.on('close', function(code) {
+    gutil.log('closing code:', gutil.colors.cyan(code));
     cb();
   });
+});
+
+/**
+* Lint with ESLint
+*/
+gulp.task('lint.check', function() {
+  return gulp.src(['**/*.js','!node_modules/**'])
+    .pipe(eslint({ configFile: '.eslintrc' }))
+    .pipe(eslint.format())
+    .pipe(eslint.results(function(results) {
+        gutil.log('Total Results: ' + results.length);
+        gutil.log('Total Warnings: ' + results.warningCount);
+        gutil.log('Total Errors: ' + results.errorCount);
+    }));
 });
