10b196847e7cf0faff8dbffde555a62d6f49fc44
umatoma <umatomakun@gmail.com>
2015-11-27 00:10:47 +0900

    create posts table migration

---
+++ migrations/20151126235610-create-posts-table.js
@@ -0,0 +1,37 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    queryInterface.createTable(
+      'posts',
+      {
+        id: {
+          type: Sequelize.INTEGER.UNSIGNED,
+          primaryKey: true,
+          autoIncrement: true
+        },
+        board_id: {
+          type: Sequelize.INTEGER.UNSIGNED,
+          allowNull: false
+        },
+        message: {
+          type: Sequelize.STRING,
+          allowNull: false
+        },
+        created_at: {
+          type: Sequelize.DATE,
+          allowNull: false
+        },
+        updated_at: {
+          type: Sequelize.DATE,
+          allowNull: false
+        }
+      }
+    );
+    queryInterface.addIndex('posts', ['board_id']);
+  },
+
+  down: function (queryInterface, Sequelize) {
+    queryInterface.dropTable('posts');
+  }
+};
